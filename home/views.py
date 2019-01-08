ebc0fba6c98cd1b56f39b9f793a19b1d5c16ad33
Saurav <sp.gharti@gmail.com>
2019-01-08 19:52:32 +0000

    remove package opencv-python

--- home/views.py
+++ home/views.py
@@ -7,7 +7,6 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
-	{'name':'opencv-python', 'url': 'http://pypi.python.org/pypi/opencv-python/3.4.0.12'},
 	{'name':'djangorestframework', 'url': 'http://pypi.python.org/pypi/djangorestframework/3.7.7'},
 	{'name':'django-bootstrap4', 'url': 'http://pypi.python.org/pypi/django-bootstrap4/0.0.5'},
     ]
